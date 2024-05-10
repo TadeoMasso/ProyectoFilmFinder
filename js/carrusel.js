468cf334554fef7e3e627aa66b382ad4a72ef7e2
Z0mbiE1 <marcosguinder7@gmail.com>
2024-05-10 19:07:18 -0300

    agregada la logica para reinicio de carrusel

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,31 +1,36 @@
-var multipleCardCarousel = document.querySelector(
-    "#carouselExampleControls"
-  );
-  if (window.matchMedia("(min-width: 768px)").matches) {
+var multipleCardCarousel = document.querySelector("#carouselExampleControls");
+
+if (window.matchMedia("(min-width: 768px)").matches) {
     var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-      interval: false,
+        interval: false,
     });
+
     var carouselWidth = $(".carousel-inner")[0].scrollWidth;
     var cardWidth = $(".carousel-item").width();
     var scrollPosition = 0;
+
+    // Logica para regresar al primer elemento del carrusel
+
     $("#carouselExampleControls .carousel-control-next").on("click", function () {
-      if (scrollPosition < carouselWidth - cardWidth * 4) {
-        scrollPosition += cardWidth;
-        $("#carouselExampleControls .carousel-inner").animate(
-          { scrollLeft: scrollPosition },
-          60
-        );
-      }
+        if (scrollPosition < carouselWidth - cardWidth * 4) {
+            scrollPosition += cardWidth;
+            $("#carouselExampleControls .carousel-inner").animate(
+                { scrollLeft: scrollPosition },
+                60
+            );
+        } else {
+            // Si estamos en la última diapositiva, regresamos al primer elemento
+            scrollPosition = 0;
+            $("#carouselExampleControls .carousel-inner").animate(
+                { scrollLeft: scrollPosition },
+                60
+            );
+        }
     });
-    $("#carouselExampleControls .carousel-control-prev").on("click", function () {
-      if (scrollPosition > 0) {
-        scrollPosition -= cardWidth;
-        $("#carouselExampleControls .carousel-inner").animate(
-          { scrollLeft: scrollPosition },
-          60
-        );
-      }
-    });
-  } else {
+
+} else {
     $(multipleCardCarousel).addClass("slide");
-  }
+}
+
+
+
