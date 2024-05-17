5aab356ed5a80952837b29c5b1d2cd12d98293e5
TadeoMasso <massotadeo@gmail.com>
2024-05-17 15:27:36 -0300

    Eliminacion de archivos que no sirven y acomode un tqoue el codigo

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,54 +1,50 @@
 var multipleCardCarousel = document.querySelector("#carouselExampleControls");
 
 if (window.matchMedia("(min-width: 768px)").matches) {
-    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-        interval: false,
-    });
+  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+    interval: false,
+  });
 
-    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-    var cardWidth = $(".carousel-item").width();
-    var scrollPosition = 0;
+  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+  var cardWidth = $(".carousel-item").width();
+  var scrollPosition = 0;
 
-    // Logica para regresar al primer elemento del carrusel
+  // Logica para regresar al primer elemento del carrusel
 
-    $("#carouselExampleControls .carousel-control-next").on("click", function () {
-        if (scrollPosition < carouselWidth - cardWidth * 4) {
-            scrollPosition += cardWidth;
-            $("#carouselExampleControls .carousel-inner").animate(
-                { scrollLeft: scrollPosition },
-                60
-            );
-        } else {
-            // Si estamos en la última diapositiva, regresamos al primer elemento
-            scrollPosition = 0;
-            $("#carouselExampleControls .carousel-inner").animate(
-                { scrollLeft: scrollPosition },
-                60
-            );
-        }
-    });
+  $("#carouselExampleControls .carousel-control-next").on("click", function () {
+    if (scrollPosition < carouselWidth - cardWidth * 4) {
+      scrollPosition += cardWidth;
+      $("#carouselExampleControls .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        60
+      );
+    } else {
+      // Si estamos en la última diapositiva, regresamos al primer elemento
+      scrollPosition = 0;
+      $("#carouselExampleControls .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        60
+      );
+    }
+  });
 
-     // Logica para moverte hacia la izquierda
-     $("#carouselExampleControls .carousel-control-prev").on("click", function () {
-        if (scrollPosition > 0) {
-            scrollPosition -= cardWidth;
-            $("#carouselExampleControls .carousel-inner").animate(
-                { scrollLeft: scrollPosition },
-                60
-            );
-        } else {
-            // Si estamos en la primera diapositiva, vamos al último elemento
-            scrollPosition = carouselWidth - cardWidth * 4;
-            $("#carouselExampleControls .carousel-inner").animate(
-                { scrollLeft: scrollPosition },
-                60
-            );
-        }
-    });
-
+  // Logica para moverte hacia la izquierda
+  $("#carouselExampleControls .carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $("#carouselExampleControls .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        60
+      );
+    } else {
+      // Si estamos en la primera diapositiva, vamos al último elemento
+      scrollPosition = carouselWidth - cardWidth * 4;
+      $("#carouselExampleControls .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        60
+      );
+    }
+  });
 } else {
-    $(multipleCardCarousel).addClass("slide");
+  $(multipleCardCarousel).addClass("slide");
 }
-
-
-
--- js/validacion.js
+++ js/validacion.js
@@ -1,147 +1,155 @@
 // Event listeners para los campos de entrada
-const nombreInput = document.getElementById('nombres');
-nombreInput.addEventListener('blur', validarNombre);
+const nombreInput = document.getElementById("nombres");
+nombreInput.addEventListener("blur", validarNombre);
 
-const apellidosInput = document.getElementById('apellidos');
-apellidosInput.addEventListener('blur', validarApellidos);
+const apellidosInput = document.getElementById("apellidos");
+apellidosInput.addEventListener("blur", validarApellidos);
 
-const correoInput = document.getElementById('correo');
-correoInput.addEventListener('blur', validarCorreo);
+const correoInput = document.getElementById("correo");
+correoInput.addEventListener("blur", validarCorreo);
 
-const textareaInput = document.getElementById('mensaje');
-textareaInput.addEventListener('blur', validarTextarea);
+const textareaInput = document.getElementById("mensaje");
+textareaInput.addEventListener("blur", validarTextarea);
 
 // Todas las Funciones de validación
 
 function validarCampoVacio(inputElement, errorElement) {
-    const valor = inputElement.value.trim();
+  const valor = inputElement.value.trim();
 
-    if (valor === '') {
-        document.getElementById(errorElement).textContent = 'Este campo es obligatorio.';
-        return false;
-    } else {
-        document.getElementById(errorElement).textContent = '';
-        return true;
-    }
+  if (valor === "") {
+    document.getElementById(errorElement).textContent =
+      "Este campo es obligatorio.";
+    return false;
+  } else {
+    document.getElementById(errorElement).textContent = "";
+    return true;
+  }
 }
 
 function validarNombre() {
-    const nombreInput = document.getElementById('nombres');
-    const nombreValido = validarCampoVacio(nombreInput, 'error_nombre');
+  const nombreInput = document.getElementById("nombres");
+  const nombreValido = validarCampoVacio(nombreInput, "error_nombre");
 
-    if (!nombreValido) {
-        return false;
-    }
+  if (!nombreValido) {
+    return false;
+  }
 
-    const nombre = nombreInput.value.trim();
+  const nombre = nombreInput.value.trim();
 
-    if (nombre === '') {
-        document.getElementById('error_nombre').textContent = 'Nombre incorrecto, no seas bobo';
-        return false;
-    } 
-    const regexNombres = /^[a-zA-Z\s]+$/;
+  if (nombre === "") {
+    document.getElementById("error_nombre").textContent =
+      "Nombre incorrecto, no seas bobo";
+    return false;
+  }
+  const regexNombres = /^[a-zA-Z\s]+$/;
 
-    if (!regexNombres.test(nombre)) {
-        document.getElementById('error_nombre').textContent = 'Ingresa un nombre válido.';
-        return false;
-    } else {
-        document.getElementById('error_nombre').textContent = '';
-        return true;
-    }
+  if (!regexNombres.test(nombre)) {
+    document.getElementById("error_nombre").textContent =
+      "Ingresa un nombre válido.";
+    return false;
+  } else {
+    document.getElementById("error_nombre").textContent = "";
+    return true;
+  }
 }
 
 function validarApellidos() {
-    const apellidosInput = document.getElementById('apellidos');
-    const apellidos = apellidosInput.value.trim();
+  const apellidosInput = document.getElementById("apellidos");
+  const apellidos = apellidosInput.value.trim();
 
-    if (!validarCampoVacio(apellidosInput, 'error_apellido')) {
-        return false;
-    }
+  if (!validarCampoVacio(apellidosInput, "error_apellido")) {
+    return false;
+  }
 
-    if (apellidos === '') {
-        document.getElementById('error_apellido').textContent = 'Apellido incorrecto, alto corki';
-        return false;
-    } 
-    const regexApellidos = /^[a-zA-Z\s]+$/;
+  if (apellidos === "") {
+    document.getElementById("error_apellido").textContent =
+      "Apellido incorrecto, alto corki";
+    return false;
+  }
+  const regexApellidos = /^[a-zA-Z\s]+$/;
 
-    if (!regexApellidos.test(apellidos)) {
-        document.getElementById('error_apellido').textContent = 'Ingresa un apellido válido.';
-        return false;
-    } else {
-        document.getElementById('error_apellido').textContent = '';
-        return true;
-    }
+  if (!regexApellidos.test(apellidos)) {
+    document.getElementById("error_apellido").textContent =
+      "Ingresa un apellido válido.";
+    return false;
+  } else {
+    document.getElementById("error_apellido").textContent = "";
+    return true;
+  }
 }
 
-
 function validarCorreo() {
-    const correoInput = document.getElementById('correo');
-    const correo = correoInput.value.trim();
+  const correoInput = document.getElementById("correo");
+  const correo = correoInput.value.trim();
 
-    if (!validarCampoVacio(correoInput, 'error_correo')) {
-        return false;
-    }
+  if (!validarCampoVacio(correoInput, "error_correo")) {
+    return false;
+  }
 
-    const regexCorreo = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const regexCorreo = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    if (!regexCorreo.test(correo)) {
-        document.getElementById('error_correo').textContent = 'Ingresa un correo válido.';
-        return false;
-    } else {
-        document.getElementById('error_correo').textContent = '';
-        return true;
-    }
+  if (!regexCorreo.test(correo)) {
+    document.getElementById("error_correo").textContent =
+      "Ingresa un correo válido.";
+    return false;
+  } else {
+    document.getElementById("error_correo").textContent = "";
+    return true;
+  }
 }
 
 function validarTextarea() {
-    const textareaInput = document.getElementById('mensaje');
-    const mensaje = textareaInput.value.trim();
+  const textareaInput = document.getElementById("mensaje");
+  const mensaje = textareaInput.value.trim();
 
-    if (!validarCampoVacio(textareaInput, 'error_mensaje') ) {
-        return false;
-    }
+  if (!validarCampoVacio(textareaInput, "error_mensaje")) {
+    return false;
+  }
 
-    if (mensaje === '') {
-        document.getElementById('error_mensaje').textContent  = 'Por favor, ingrese un mensaje.';
-        return false;
-    } else {
-        document.getElementById('error_mensaje').textContent = '';
-        return true;
-    }
+  if (mensaje === "") {
+    document.getElementById("error_mensaje").textContent =
+      "Por favor, ingrese un mensaje.";
+    return false;
+  } else {
+    document.getElementById("error_mensaje").textContent = "";
+    return true;
+  }
 }
 
 // Validación al enviar el formulario
-const formulario = document.querySelector('form');
-formulario.addEventListener('submit', validarFormulario);
+const formulario = document.querySelector("form");
+formulario.addEventListener("submit", validarFormulario);
 
 function validarFormulario(event) {
-    event.preventDefault(); // Evita que el formulario se envíe automáticamente
+  event.preventDefault(); // Evita que el formulario se envíe automáticamente
 
-    // Llama a las funciones de validación para cada campo
-    const nombreValido = validarNombre();
-    const apellidosValidos = validarApellidos();
-    const correoValido = validarCorreo();
-    const mensajeValido = validarTextarea();
+  // Llama a las funciones de validación para cada campo
+  const nombreValido = validarNombre();
+  const apellidosValidos = validarApellidos();
+  const correoValido = validarCorreo();
+  const mensajeValido = validarTextarea();
 
-    // Verifica si se aceptaron los términos y condiciones
-const terminosCheckbox = document.getElementById('terminos');
-if (!terminosCheckbox.checked) {
-  document.getElementById('error_terminos').textContent = 'Debes aceptar los términos y condiciones';
-  return; // Si los términos no están aceptados, no envíes el formulario
-} else {
-  document.getElementById('error_terminos').textContent = '';
-}
+  // Verifica si se aceptaron los términos y condiciones
+  const terminosCheckbox = document.getElementById("terminos");
+  if (!terminosCheckbox.checked) {
+    document.getElementById("error_terminos").textContent =
+      "Debes aceptar los términos y condiciones";
+    return; // Si los términos no están aceptados, no envíes el formulario
+  } else {
+    document.getElementById("error_terminos").textContent = "";
+  }
 
-// Si alguno de los campos no es válido, no envíes el formulario
-if (!nombreValido || !apellidosValidos || !correoValido || !mensajeValido || !terminosCheckbox) {
-  return;
-}
+  // Si alguno de los campos no es válido, no envíes el formulario
+  if (
+    !nombreValido ||
+    !apellidosValidos ||
+    !correoValido ||
+    !mensajeValido ||
+    !terminosCheckbox
+  ) {
+    return;
+  }
 
-// Si todos los campos son válidos y se aceptaron los términos, envía el formulario
-formulario.submit();
-
-  
-
-
-
+  // Si todos los campos son válidos y se aceptaron los términos, envía el formulario
+  formulario.submit();
 }
